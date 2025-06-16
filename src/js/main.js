d122ae5ba2ccefcc69e591c3d8758a9fae70e107
d-yv <yuri.dubrovin@gmail.com>
2025-06-17 00:45:35 +0300

    add flatpickr

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,38 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 import { renderTasks } from './render.js';
 
 const categories = ['Работа', 'Дом', 'Учёба', 'Спорт'];
-const assignees = ['Я', 'Катя', 'Команда', 'Не важно'];
+const assignees = ['Юра', 'Катя', 'Таня', 'Надя'];
+
+const date = document.querySelector('#deadline');
+
+let userSelectedDate = 0;
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+
+  onClose(selectedDates) {
+    const dateNow = new Date();
+    let dateVerify = selectedDates[0];
+    if (dateVerify > dateNow) {
+      userSelectedDate = dateVerify;
+    } else {
+      iziToast.show({
+        title: 'Error',
+        titleColor: 'red',
+        message: 'Please choose a date in the future',
+        position: 'topRight',
+      });
+    }
+  },
+};
+
+flatpickr(date, options);
 
 function populateSelect(selectElement, options) {
   options.forEach(option => {
@@ -21,11 +52,10 @@ const form = document.querySelector('#taskForm');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
-
   const title = document.querySelector('#title').value.trim();
   const category = document.querySelector('#categorySelect').value;
   const deadline = document.querySelector('#deadline').value;
-  const assignedTo = document.querySelector('#assigneeSelect').value.trim();
+  const assignedTo = document.querySelector('#assigneeSelect').value;
 
   const task = {
     id: Date.now(),
